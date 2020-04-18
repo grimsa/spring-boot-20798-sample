03a6a72caef934f1ac2caf38e42c7f7f298e040e
grimsa <gediminas.rimsa@gmail.com>
2020-04-18 10:32:57 +0300

    Simplified some more

--- src/main/java/com/example/demo/domain/ProjectManagerImpl.java
+++ src/main/java/com/example/demo/domain/ProjectManagerImpl.java
@@ -3,27 +3,12 @@ package com.example.demo.domain;
 import com.example.demo.domain.a.ProjectManager;
 import com.example.demo.domain.entities.BaseUser;
 import com.example.demo.domain.entities.Project;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @Transactional
-public class ProjectManagerImpl implements ProjectManager, InitializingBean {
-    private ProjectDao dao;
-
-    public ProjectManagerImpl() {
-    }
-
-    public void setDao(ProjectDao dao) {
-        this.dao = dao;
-    }
-
-    @Override
-    public void afterPropertiesSet() {
-        System.out.println("Bean created");
-    }
-
+public class ProjectManagerImpl implements ProjectManager {
     @Override
     public void doStuff(Project withThis) {
         System.out.println("Doing something" + withThis.getCreationDate());
