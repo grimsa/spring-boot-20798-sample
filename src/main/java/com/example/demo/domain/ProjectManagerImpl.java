2cad1af48336673099e2c39448d2dc3a86a82d2d
grimsa <gediminas.rimsa@gmail.com>
2020-04-18 10:36:37 +0300

    Simplified some more

--- src/main/java/com/example/demo/domain/ProjectManagerImpl.java
+++ src/main/java/com/example/demo/domain/ProjectManagerImpl.java
@@ -4,21 +4,13 @@ import com.example.demo.domain.a.ProjectManager;
 import com.example.demo.domain.entities.BaseUser;
 import com.example.demo.domain.entities.Project;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
-@Transactional
 public class ProjectManagerImpl implements ProjectManager {
     @Override
     public void doStuff(Project withThis) {
         System.out.println("Doing something" + withThis.getCreationDate());
         doSomethingWithOtherEntity(withThis.creator());
-    }
-
-    @Override
-    public Project doStuffWithProject() {
-        System.out.println("doing stuff ");
-        return null;
     }
 
     public void doSomethingWithOtherEntity(BaseUser e) {
--- src/main/java/com/example/demo/domain/a/ProjectManager.java
+++ src/main/java/com/example/demo/domain/a/ProjectManager.java
@@ -2,10 +2,6 @@ package com.example.demo.domain.a;
 
 import com.example.demo.domain.b.Service;
 import com.example.demo.domain.entities.Project;
-import org.springframework.transaction.annotation.Transactional;
 
 public interface ProjectManager extends Service<Project> {
-
-    @Transactional
-    Project doStuffWithProject();
 }
