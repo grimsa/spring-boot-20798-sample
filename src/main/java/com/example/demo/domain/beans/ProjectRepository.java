a54ac37f9cf21fae43c8c80fb00fb1d6ad525d8e
grimsa <gediminas.rimsa@gmail.com>
2020-04-18 10:47:30 +0300

    Simplified some more

--- src/main/java/com/example/demo/domain/beans/ProjectRepository.java
+++ src/main/java/com/example/demo/domain/beans/ProjectRepository.java
@@ -1,9 +1,9 @@
 package com.example.demo.domain.beans;
 
 import com.example.demo.domain.entities.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProjectRepository extends JpaRepository<Project, Long> {
+public interface ProjectRepository extends CrudRepository<Project, Long> {
 }
