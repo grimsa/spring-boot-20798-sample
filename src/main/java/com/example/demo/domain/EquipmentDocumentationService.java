712f5afd72e50fa16fd54fc633e9e1a220d68c7c
grimsa <gediminas.rimsa@gmail.com>
2020-04-18 10:28:40 +0300

    Simplified more

--- src/main/java/com/example/demo/domain/EquipmentDocumentationService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.domain;
-
-import com.example.demo.domain.entities.Project;
-
-import java.util.List;
-
-public interface EquipmentDocumentationService {
-    List<String> something(Project project);
-}
