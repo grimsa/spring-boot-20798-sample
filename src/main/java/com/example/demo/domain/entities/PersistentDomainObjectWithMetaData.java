c007f780599d269b51681ef3f15d1eee0e870604
grimsa <gediminas.rimsa@gmail.com>
2020-04-18 10:49:39 +0300

    Simplified some more

--- src/main/java/com/example/demo/domain/entities/PersistentDomainObjectWithMetaData.java
+++ src/main/java/com/example/demo/domain/entities/PersistentDomainObjectWithMetaData.java
@@ -4,9 +4,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import java.util.Date;
 
 @MappedSuperclass
 public class PersistentDomainObjectWithMetaData {
@@ -14,22 +11,11 @@ public class PersistentDomainObjectWithMetaData {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    Date creationDate = new Date();
-
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public Date getCreationDate() {
-        return this.creationDate;
-    }
-
-    public void setCreationDate(Date creationDate) {
-        this.creationDate = creationDate;
     }
 }
